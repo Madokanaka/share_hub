f5ba882fab36977b31279c88152016f98ec029bf
Seitek <seita20078@gmail.com>
2025-05-03 11:40:06 +0600

    initial commit

---
+++ src/main/java/org/attractor/java_share_hub/JavaShareHubApplication.java
@@ -0,0 +1,13 @@
+package org.attractor.java_share_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShareHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaShareHubApplication.class, args);
+    }
+
+}
