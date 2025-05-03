3cfac3856521ff56a9108642a09f5e6d7abb5fbf
Seitek <seita20078@gmail.com>
2025-05-03 12:21:12 +0600

    made auth controller

---
+++ src/main/java/org/attractor/java_share_hub/controller/AuthController.java
@@ -0,0 +1,59 @@
+package org.attractor.java_share_hub.controller;
+
+import jakarta.validation.Valid;
+import org.attractor.java_share_hub.dto.UserDto;
+import org.attractor.java_share_hub.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.UnsupportedEncodingException;
+
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("userDto", new UserDto());
+        return "auth/registration";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage(Model model, @RequestParam(required = false) String error) {
+        if (error != null) {
+            model.addAttribute("error", error);
+        }
+        return "auth/login";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid @ModelAttribute UserDto userDto,
+                               BindingResult bindingResult,
+                               Model model) {
+        if (userService.existsByEmail(userDto.getEmail())) {
+            bindingResult.rejectValue("email", "email.exists", "User with this email already exists");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("userDto", userDto);
+            return "auth/registration";
+        }
+
+        userService.registerUser(userDto);
+
+        return "redirect:/auth/login";
+    }
+
+}
+
